5bb0066984382545ce530ab74f7183cbdc296651
Diego <mondragdiego@gmail.com>
2015-12-21 16:43:29 -0800

    Exercises shown in the C# Fundamentals tutorial

---
+++ GradeBook/GradeBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class GradeBook
+    {
+        private List<float> grades;
+        public string Name;
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+    
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+    }
+}
